8833ec3dd4e8cf8b97f5326d1e5115e4406f16a6
imadcollin <imad.collin@gmail.com>
2021-08-12 23:10:06 +0200

    Initial Commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> React Native </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+});
+
+export default App;
