ef9cc3b1065836c514689419b37112343de08f41
imadcollin <imad.collin@gmail.com>
2021-08-12 23:52:12 +0200

    Add radio player package

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import RadioPlayer, {
+} from 'react-native-radio-player';
 const App = () => {
+  const playing = () => {
+    console.log('playing...');
+    let x = 'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one';
+    const bbc_3 = 'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_three';
+    RadioPlayer.radioURL(bbc_3);
+
+    RadioPlayer.play();
+  };
+  const stop = () => {
+    RadioPlayer.stop();
+  };
+
   return (
     <SafeAreaView>
       <View>
         <Text> React Native </Text>
+        <Button title="Play" onPress={playing}></Button>
+        <Button title="Stop" color="#ff0000" onPress={stop}></Button>
       </View>
     </SafeAreaView>
   );
