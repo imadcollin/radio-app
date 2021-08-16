beb56ef7f32f766873c8115f65383d879c3536ba
imadcollin <imad.collin@gmail.com>
2021-08-16 16:53:38 +0200

    Map channels to buttons

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import RadioPlayer, {
-} from 'react-native-radio-player';
+import Channels from './helper/channels';
+import RadioPlayer from 'react-native-radio-player';
 const App = () => {
+  const [isPLaying, setIsPlaying] = useState(false);
   const playing = () => {
-    console.log('playing...');
-    let x = 'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one';
-    const bbc_3 = 'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_three';
-    RadioPlayer.radioURL(bbc_3);
-
+    if (!isPLaying) {
+      RadioPlayer.stop();
+    }
+    RadioPlayer.radioURL(Channels[0].url);
     RadioPlayer.play();
+    setIsPlaying(!isPLaying);
   };
   const stop = () => {
     RadioPlayer.stop();
@@ -19,7 +20,9 @@ const App = () => {
     <SafeAreaView>
       <View>
         <Text> React Native </Text>
-        <Button title="Play" onPress={playing}></Button>
+        {Channels.map((item) => (
+          <Button title={item.name} key={item.id} onPress={playing}></Button>
+        ))}
         <Button title="Stop" color="#ff0000" onPress={stop}></Button>
       </View>
     </SafeAreaView>
