f461da62acc3c889889433e422d15be0b2320e09
imadcollin <imad.collin@gmail.com>
2021-08-16 16:53:06 +0200

    Add data file

---
+++ helper/channels.js
@@ -0,0 +1,22 @@
+const Channels = [
+  {
+    id: 1,
+    name: 'channel 1',
+    url:'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one',
+    img: ''
+},
+{
+    id: 2,
+    name: 'channel 2',
+    url: 'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_two',
+    img: ''
+  },
+  {
+    id: 3,
+    name: 'channel 3',
+    url: 'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_three',
+    img: ''
+  },
+];
+
+export default  Channels; 
