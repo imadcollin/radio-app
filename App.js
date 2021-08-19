1e29863f7e418c83515542973aec872d3b435a16
imadcollin <imad.collin@gmail.com>
2021-08-19 17:23:34 +0200

    Add logo to each channels

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {SafeAreaView, Text, View} from 'react-native';
 import Channels from './helper/channels';
 import RadioPlayer from 'react-native-radio-player';
 import {Button, ThemeProvider} from 'react-native-elements';
+import {Card, ListItem, Icon, Image} from 'react-native-elements';
 
 const App = () => {
   const [isPLaying, setIsPlaying] = useState(false);
@@ -28,20 +29,42 @@ const App = () => {
   return (
     <SafeAreaView>
       <View>
-        <Text> React Native </Text>
-        {Channels.map(item => (
-          <Button
-            title={item.name}
-            key={item.id}
-            onPress={() => playing(item.url)}></Button>
-        ))}
-        <ThemeProvider theme={theme}>
-          <Button
-            type="outline"
-            title="Stop"
-            color="red"
-            onPress={stop}></Button>
-        </ThemeProvider>
+        <Card>
+          <Card.Title>Channels</Card.Title>
+          <Card.Divider />
+          {Channels.map((channel, i) => {
+            return (
+              <ListItem
+                key={i}
+                bottomDivider
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <Image
+                  style={{width: 50, height: 40}}
+                  resizeMode="cover"
+                  source={channel.logo}
+                />
+                <Text>{channel.name} </Text>
+                <Icon
+                  name="play-circle-outline"
+                  type="ionicon"
+                  color="#517fa4"
+                  onPress={() => playing(channel.url)}
+                />
+              </ListItem>
+            );
+          })}
+          <ThemeProvider theme={theme}>
+            <Button
+              type="outline"
+              title="Stop"
+              color="red"
+              onPress={stop}></Button>
+          </ThemeProvider>
+        </Card>
       </View>
     </SafeAreaView>
   );
