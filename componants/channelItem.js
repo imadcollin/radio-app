a1274a3b754a900891b641837b3661bafb24a205
imadcollin <imad.collin@gmail.com>
2021-08-24 23:52:14 +0200

    Move storage code block to app

--- componants/channelItem.js
+++ componants/channelItem.js
@@ -2,29 +2,9 @@ import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import {Button} from 'react-native-elements';
 import {Card, ListItem, Icon, Image} from 'react-native-elements';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ChannelItem = ({channel, playing}) => {
+const ChannelItem = ({channel, playing,stt}) => {
   const [starName, setStarName] = useState('star-outline');
-  const st = async value => {
-    try {
-      await AsyncStorage.setItem('@storage_key', value);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  const stt = value => {
-    console.log('read', value);
-    st(value);
-  };
-  const rt = async () => {
-    try {
-      const test = await AsyncStorage.getItem('@storage_key');
-      console.log('retrieved', test);
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
   const fav = e => {
     console.log(channel.url);
