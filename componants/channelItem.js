80f1a60dccede150e31103d57964db8553ddb5eb
imadcollin <imad.collin@gmail.com>
2021-08-30 22:59:01 +0200

    Fix space btw items

--- componants/channelItem.js
+++ componants/channelItem.js
@@ -1,46 +1,45 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
-import {Button} from 'react-native-elements';
-import {Card, ListItem, Icon, Image} from 'react-native-elements';
+import {View, StyleSheet, Text} from 'react-native';
+import {ListItem, Icon, Image} from 'react-native-elements';
 
 const ChannelItem = ({channel, playing, handleFav}) => {
   const [starName, setStarName] = useState('star-outline');
 
   const fav = e => {
     setStarName(starName === 'star' ? 'star-outline' : 'star');
-    handleFav(channel,starName);
+    handleFav(channel, starName);
   };
   return (
     <View>
-      <ListItem
-        key={channel.id}
-        bottomDivider
-        style={{
-          display: 'flex',
-          flexDirection:'column',
-          alignItems: 'stretch',
-          justifyContent:'flex-end',
-          padding: 10,
-        }}>
-        <Image
-          style={{width: 50, height: 40}}
-          resizeMode="cover"
-          source={channel.logo}
-        />
-        <Text>{channel.name} </Text>
-        <Icon
-          name="play-circle-outline"
-          type="ionicon"
-          color="#517fa4"
-          onPress={() => playing(channel.url)}
-        />
-        <Icon
-          name={starName}
-          type="ionicon"
-          onPress={fav}
-        />
+      <ListItem key={channel.id} bottomDivider>
+        <View style={itemsStyle.container}>
+          <Image
+            style={itemsStyle.img}
+            resizeMode="cover"
+            source={channel.logo}
+          />
+          <Text>{channel.name} </Text>
+          <Icon
+            name="play-circle-outline"
+            type="ionicon"
+            color="#517fa4"
+            onPress={() => playing(channel.url)}
+          />
+          <Icon name={starName} type="ionicon" onPress={fav} />
+        </View>
       </ListItem>
     </View>
   );
 };
 export default ChannelItem;
+
+const itemsStyle = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexGrow: 1,
+    width: 'auto',
+    justifyContent: 'space-between',
+  },
+  img: {width: 50, height: 40},
+});
