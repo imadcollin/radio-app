a00b7c087c13c1c8dd126d5720c7765a45d0741e
imadcollin <imad.collin@gmail.com>
2021-09-01 23:16:43 +0200

    Fix the slider bug not changing volume

--- componants/volumerSlider.js
+++ componants/volumerSlider.js
@@ -1,23 +1,52 @@
-import React,{useState} from 'react';
-import {View, Text} from 'react-native';
-import VolumeControl, {VolumeControlEvents} from 'react-native-volume-control';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import VolumeControl from 'react-native-volume-control';
 import Slider from '@react-native-community/slider';
 import {Icon} from 'react-native-elements';
 
 const VolumeSlider = () => {
-  const [volume] = useState(0);
+  const [volume, setVolume] = useState(10);
 
   const sliderChange = value => {
     VolumeControl.change(value);
+    setVolume(value);
+  };
+  const mute = () => {
+    VolumeControl.change(0);
+  };
+  const unmute = () => {
+    VolumeControl.change(volume);
   };
   return (
-    <View>
-        <Icon name="volume-medium-outline"></Icon>
+    <View style={sty.container}>
+      <Icon
+        containerStyle={{marginTop: 8}}
+        name="volume-off"
+        onPress={mute}></Icon>
       <Slider
+        style={{width: 300, height: 40}}
+        minimumValue={0}
+        maximumValue={1}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#000000"
         value={volume}
         onValueChange={sliderChange}
       />
+      <Icon
+        containerStyle={{marginTop: 8}}
+        name="volume-up"
+        onPress={unmute}></Icon>
     </View>
   );
 };
 export default VolumeSlider;
+
+const sty = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 50,
+  },
+});
